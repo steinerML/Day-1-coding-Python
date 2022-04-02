b000ab3301cebbd297f12d8d3db6005fb6db4a06
steinerML <102678863+steinerML@users.noreply.github.com>
2022-04-02 20:10:32 +0200

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,12 @@
+
+#Historias Locas
+
+word1 = input("Enter a word that makes you feel energized: ")
+word2 = input("Enter the name of a country or region you like: ")
+word3 = input("For whom would you like to work? (big-tech, small company, startup..: ")
+word4 = input("Enter the name of another country you would like to travel to: ")
+
+
+madlib = f"Programming Python code will make me {word1}. It will allow me to work from {word2} and become more valuble for {word3} all over the world. I will be able to work from {word4} whenever I want and also earn more money!"
+
+print(madlib)
